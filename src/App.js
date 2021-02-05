4124fdb49a98b4562e5de7dc2e5f46da1995fbb2
david espinal <despinal0425@gmail.com>
2021-02-05 10:02:39 -0500

    added button and some css style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="col">
+        <div><span>A:</span><span>{props.a}</span></div>
+        <button>Update A</button>
+      </div>
+
+      <div className="col">
+        <div><span>B:</span><span>{props.b}</span></div>
+        <button>Update B</button>
+      </div>
     </div>
   );
 }
